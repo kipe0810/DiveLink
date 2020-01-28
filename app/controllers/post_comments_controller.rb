b21b131c6acd163265f03dbd547427c542910a84
kippeinakade <tkrs23280810@gmail.com>
2020-01-28 08:08:07 +0000

    add ajax

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -4,14 +4,13 @@ class PostCommentsController < ApplicationController
     @comment = current_user.post_comments.new(post_comment_params)
     @comment.post_id = @post.id
     @comment.save
-    redirect_to @post
+    @post_comment = PostComment.new
 	end
 
 	def destroy
 		PostComment.find(params[:id]).destroy
 		@post = Post.find(params[:post_id])
 		@post_comment = PostComment.new
-		redirect_to @post
 	end
 
 	private
